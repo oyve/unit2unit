180a04ff7e423decedaa4b9fb09336432e01b9a9
oyve <oyve-github@outlook.com>
2025-03-08 18:35:11 +0100

    add atomic

--- src/length.ts
+++ src/length.ts
@@ -4,14 +4,16 @@ import Metric from './length/length.metric';
 import UK from './length/length.UK';
 import US from './length/length.US';
 import Nautical from './length/length.nautical';
-import Astronomical from './length/length.astronomical';
 import Special from './length/length.special';
+import Atomic from './length/length.atomic';
+import Astronomical from './length/length.astronomical';
 
 export default {
     Metric,
     UK,
     US,
     Nautical,
-    Astronomical,
-    Special
+    Special,
+    Atomic,
+    Astronomical
 };

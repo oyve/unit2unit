a913a10a15d988c0471784ae1e319a36fe32748b
oyve <oyve-github@outlook.com>
2025-03-08 18:32:10 +0100

    remove

--- tests/length/length.test.ts
+++
@@ -1,8 +0,0 @@
-import length from '../../src/length';
-
-describe('SI Length Conversions', () => {
-    test('SI meter to US cablelength', () => {
-        const meter = length.Metric.meter(1000);
-        expect(meter.toUS().toCableLength()).toBe(4.556722076407116);
-    });
-});

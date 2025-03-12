aba344e3eb86f0f9320dc03bdc71b25f368706a1
oyve <oyve-github@outlook.com>
2025-03-12 16:00:22 +0100

    refactor

--- src/temperature.ts
+++ src/temperature.ts
@@ -1,6 +1,5 @@
-'use strict'
 import Big from "big.js";
-import { convertTo, round } from "./common";
+import { round } from "./common";
 
 const to = (kelvin: number | Big) => { //kelvin
     return {
@@ -12,15 +11,6 @@ const to = (kelvin: number | Big) => { //kelvin
         toRankine: (decimalPlaces?: number): number | Big => round(new Big(kelvin).times(9/5), decimalPlaces) || 0,
         toRéaumur: (decimalPlaces?: number): number | Big => round(new Big(kelvin).minus(273.15).times(4/5), decimalPlaces) || 0,
         toRømer: (decimalPlaces?: number): number | Big => round(new Big(kelvin).minus(273.15).times(21/40).plus(7.5), decimalPlaces) || 0,
-
-        toK: function(decimalPlaces?: number): number | Big { return this.toKelvin(decimalPlaces); },
-        toC: function(decimalPlaces?: number): number | Big  { return this.toCelsius(decimalPlaces); },
-        toF: function(decimalPlaces?: number): number | Big  { return this.toFahrenheit(decimalPlaces); },
-        toDe: function(decimalPlaces?: number): number | Big  { return this.toDelisle(decimalPlaces); },
-        toN: function(decimalPlaces?: number): number | Big  { return this.toNewton(decimalPlaces); },
-        toR: function(decimalPlaces?: number): number | Big  { return this.toRankine(decimalPlaces); },
-        toRé: function(decimalPlaces?: number): number | Big  { return this.toRéaumur(decimalPlaces); },
-        toRø: function(decimalPlaces?: number): number | Big  { return this.toRømer(decimalPlaces); }
     }
 }
 
@@ -33,13 +23,4 @@ export default {
     rankine: (value: number) => to(value * 5/9),
     réaumur: (value: number) => to(value * 5/4 + 273.15),
     rømer: (value: number) => to((value - 7.5) * 40/21 + 273.15),
-
-    K: function(value: number) { return this.kelvin(value); },
-    C: function(value: number) { return this.celsius(value); },
-    F: function(value: number) { return this.fahrenheit(value); },
-    De: function(value: number) { return this.delisle(value); },
-    N: function(value: number) { return this.newton(value); },
-    R: function(value: number) { return this.rankine(value); },
-    Ré: function(value: number) { return this.réaumur(value); },
-    Rø: function(value: number) { return this.rømer(value); },
 }
